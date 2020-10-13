cc0c7f8309a06ef2a51e17e66ab507981b220786
lancaster-coder1991 <grjscott1991@hotmail.com>
2020-10-13 16:35:04 +0100

    develop responsive styling on all elements

--- meteorite_app/src/App.js
+++ meteorite_app/src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
+import { render } from "react-dom";
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 import "./App.css";
 import Header from "./Components/Header";
 import SearchForm from "./Components/SearchForm";
 import ResultsTable from "./Components/ResultsTable";
+
+const position = [51.505, -0.09];
 
 class App extends React.Component {
   state = {
@@ -11,7 +15,7 @@ class App extends React.Component {
 
   updateMeteorites = (dropdownValue, searchText) => {
     fetch(
-      `https://data.nasa.gov/resource/gh4g-9sfh.json?${dropdownValue}=${searchText}`
+      `https://data.nasa.gov/resource/gh4g-9sfh.json?${dropdownValue}=${searchText}&$limit=20`
     )
       .then((res) => {
         return res.json();
@@ -40,12 +44,36 @@ class App extends React.Component {
       });
   }
 
+  makeMarkers = () => {
+    return this.state.meteorites.map((meteorite) => {
+      return (
+        <Marker
+          key={meteorite.id}
+          position={[meteorite.reclat, meteorite.reclong]}
+        >
+          <Popup>
+            {`Name: ${meteorite.name}`}
+            <br />
+            {`Year: ${meteorite.year.slice(0, 4)}`}
+          </Popup>
+        </Marker>
+      );
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <Header />
         <SearchForm updateMeteorites={this.updateMeteorites} />
         <ResultsTable meteorites={this.state.meteorites} />
+        <Map center={position} zoom={1}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+          {this.makeMarkers()}
+        </Map>
       </div>
     );
   }
